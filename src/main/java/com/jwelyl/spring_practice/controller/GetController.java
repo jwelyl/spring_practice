11336ef66982b4fc374d8041f06d75c267730403
jwelyl <cork2586@naver.com>
2023-01-03 01:23:52 +0900

    23.01.03 01:23 GET API, @PathVariable

--- src/main/java/com/jwelyl/spring_practice/controller/GetController.java
+++ src/main/java/com/jwelyl/spring_practice/controller/GetController.java
@@ -1,9 +1,6 @@
 package com.jwelyl.spring_practice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
@@ -18,5 +15,17 @@ public class GetController {
     @GetMapping(value = "/name")
     public String getName() {
         return "Flature";
+    }
+
+    //  http://localhost:8080/api/v1/get-api/variable1/{String 값}
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        return variable;
+    }
+
+    //  http://localhost:8080/api/v1/get-api/variable2/{String 값}
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable(value = "variable") String var) {
+        return var;
     }
 }

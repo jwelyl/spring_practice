f864b70821b488a9bb063ee5b0fbfd1d18d9af6d
jwelyl <cork2586@naver.com>
2023-01-04 23:48:43 +0900

    23.01.04 11:35 POST API, @RequestBody, Map

--- src/main/java/com/jwelyl/spring_practice/controller/PostController.java
+++ src/main/java/com/jwelyl/spring_practice/controller/PostController.java
@@ -1,14 +1,27 @@
 package com.jwelyl.spring_practice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/post-api")
 public class PostController {
+    //  http://localhost:8080/api/v1/post-api/member
     @RequestMapping(value = "/domain", method = RequestMethod.POST)
     public String postExample() {
         return "Hello Post API";
+    }
+
+    //  http://localhost:8080/api/v1/post-api/member
+    @PostMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
     }
 }

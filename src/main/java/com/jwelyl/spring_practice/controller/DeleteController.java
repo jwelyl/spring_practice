a9d89e76eff29d7ff0ee5740bdf056cab0b25fb5
허재성 <cork2586@naver.com>
2023-01-06 15:18:28 +0900

    23.01.06. 15:18 @RequestParam을 활용한 DELETE 메서드 구현

--- src/main/java/com/jwelyl/spring_practice/controller/DeleteController.java
+++ src/main/java/com/jwelyl/spring_practice/controller/DeleteController.java
@@ -1,9 +1,6 @@
 package com.jwelyl.spring_practice.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/delete-api")
@@ -12,5 +9,11 @@ public class DeleteController {
     @DeleteMapping(value = "/{variable}")
     public String deleteVariable(@PathVariable String variable) {
         return variable;
+    }
+
+    //  http://localhost:8080/api/v1/delete-api/request1?email=value
+    @DeleteMapping(value = "/request1")
+    public String getRequestParam1(@RequestParam String email) {
+        return "e-mail : " + email;
     }
 }
